9f1fc04b3c904bbaa87b80b3cfc918dc90ec73be
Student <student@Alexander-Witherspoons-MacBook-Air.local>
2018-01-30 11:54:02 -0500

    some functions for checking and fixing types

--- testing.py
+++ testing.py
@@ -1,61 +1,99 @@
-#type(), int(), & float()
-print(type(3))
-print(type(3.5))
-print(type(3.0))
-
-print(int(3.8))
-print(int(3.0))
-
-print(float(3))
-
-#fractions
 from fractions import Fraction
-f = Fraction(3, 4)
-print(f)
 
-f = Fraction (3, 4) + 1 + 1.5
-print(f)
-
-f = Fraction (3, 4) + 1 + Fraction(1, 4)
-print(f)
-
-#complex numbers
-a = 2 + 3j
-print(type(a))
-
-a = complex(2, 3)
-print(a)
-
-b = 3 + 3j
-
-print(a + b)
-print(a - b)
-print(a * b)
-print(a / b)
-
-z = 2 + 3j
-print(z.real)
-print(z.imag)
-print(z.conjugate())
-print((z.real**2 + z.imag**2)**0.5)
-print(abs(z))
+# #type(), int(), & float()
+# print(type(3))
+# print(type(3.5))
+# print(type(3.0))
+#
+# print(int(3.8))
+# print(int(3.0))
+#
+# print(float(3))
+#
+# #fractions
+# f = Fraction(3, 4)
+# print(f)
+#
+# f = Fraction (3, 4) + 1 + 1.5
+# print(f)
+#
+# f = Fraction (3, 4) + 1 + Fraction(1, 4)
+# print(f)
+#
+# #complex numbers
+# a = 2 + 3j
+# print(type(a))
+#
+# a = complex(2, 3)
+# print(a)
+#
+# b = 3 + 3j
+#
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+#
+# z = 2 + 3j
+# print(z.real)
+# print(z.imag)
+# print(z.conjugate())
+# print((z.real**2 + z.imag**2)**0.5)
+# print(abs(z))
 
 #user input
-#NONE OF THIS WORKS
-#things that do not work as advertised in book:
-#inputting "3/4" into int() or float() doesn't raise a ValueError
-# it just thinks "3/4" = 0
-#inputting a float into int() doesn't raise a ValueError either
-# it just rounds the float down
-#unable to get .is_integer to work, threw me a NameError??
-#the Fraction(input()) thing doesn't work either
-# fractions entered in format "numerator/denominator" just equal 0
-#so of course I couldn't test the ZeroDivisionError thing either
-#this is so frustrating!!
-#I blame all of this on you Steve
 
-z = complex(input('Enter a complex number: '))
-print(z)
+# try:
+#     a = float(input('Enter a number: '))
+# except ValueError:
+#     print('You entered an invalid number')
 
-#the above works, but it doesn't throw a ValueError if you put spaces in your complex number
-#("3 + 2j" vs "3+2j") like the book says it does
+# def check_int(a):
+#     try:
+#         a = float(a)
+#         if a.is_integer() == True:
+#             a = int(a)
+#             print('%s is a valid integer' % a)
+#         else:
+#             print('You entered an invalid integer')
+#     except ValueError:
+#         try:
+#             a = int(a)
+#             print('%s is a valid integer' % a)
+#         except ValueError:
+#             print('You entered an invalid integer')
+#
+# a = check_int(input('Please enter an integer: '))
+
+
+def check_Fraction(a):
+    from fractions import Fraction
+    try:
+        a = Fraction(a)
+        print(a)
+    except ValueError:
+        print('You entered an invalid fraction')
+    except ZeroDivisionError:
+        print('You entered an invalid fraction')
+
+a = check_Fraction(input('Enter a fraction: '))
+
+# z = input('Enter a complex number: ')
+#
+# try:
+#     z = complex(z)
+#     print(z)
+# except ValueError:
+#     z = z.replace(' ', '')
+#     z = z.replace('i', 'j')
+#     try:
+#         z = complex(z)
+#         print(z)
+#     except ValueError:
+#         print('You entered an invalid complex number')
+#
+# def is_factor(a, b):
+#     if b % a == 0:
+#         return True
+#     else:
+#         return False
