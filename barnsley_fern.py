1d602187dad4a9646ddba996d292ec8e4a06843d
Student <student@Alexander-Witherspoons-MacBook-Air.local>
2018-05-10 11:28:34 -0400

    Barnsley fern

---
+++ barnsley_fern.py
@@ -0,0 +1,67 @@
+import random
+import matplotlib.pyplot as plt
+
+def transformation1(point):
+    x = point[0]
+    y = point[1]
+    x1 = 0.85*x + 0.04*y
+    y1 = -0.04*x + 0.85*y + 1.6
+    return x1, y1
+
+def transformation2(point):
+    x = point[0]
+    y = point[1]
+    x1 = 0.2*x - 0.26*y
+    y1 = 0.23*x + 0.22*y + 1.6
+    return x1, y1
+
+def transformation3(point):
+    x = point[0]
+    y = point[1]
+    x1 = -0.15*x + 0.28*y
+    y1 = 0.26*x + 0.24*y + 0.44
+    return x1, y1
+
+def transformation4(point):
+    x = point[0]
+    y = point[1]
+    x1 = 0
+    y1 = 0.16*y
+    return x1, y1
+
+def get_index(probability):
+    r = random.random()
+    cumulProb = 0
+    sumProb = []
+    for p in probability:
+        cumulProb += p
+        sumProb.append(cumulProb)
+    for index, sp in enumerate(sumProb):
+        if r <= sp:
+            return index
+    return len(probability)-1
+
+def transform(point):
+    transformations = [transformation1, transformation2, transformation3, transformation4]
+    probability = [0.85, 0.07, 0.07, 0.01]
+    transIndex = get_index(probability)
+    transFunct = transformations[transIndex]
+    x, y = transFunct(point)
+    return x, y
+
+def draw_fern(num):
+     x = [0]
+     y = [0]
+
+     x1, y1 = 0, 0
+     for i in range(num):
+         x1, y1 = transform((x1, y1))
+         x.append(x1)
+         y.append(y1)
+     return x, y
+
+num = int(input('Enter the number of points in the fern: '))
+x, y = draw_fern(num)
+plt.plot(x, y, 'go', markersize=0.01)
+plt.title('Fern with {0} points'.format(num))
+plt.show()
